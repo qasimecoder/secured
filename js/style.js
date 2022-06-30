2ad78aabe320b0ce380ba5a3a82ab03c71c60c5a
qasimecoder <qasimecoder123@gmail.com>
2022-07-01 02:49:56 +0500

    file upload

---
+++ js/style.js
@@ -0,0 +1,258 @@
+$('.cus_four2').slick({
+    dots: false,
+    arrows:true,
+    infinite:true,
+    autoplay:true,
+    speed: 300,
+    slidesToShow:3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1 
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  // 2nd slider 
+
+  $('.cus_comp1').slick({
+    dots: false,
+    arrows:false,
+    infinite:true,
+    autoplay:true,
+    speed: 300,
+    slidesToShow:4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1 
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+  $('.cus_nine5').slick({
+    dots: false,
+    arrows:false,
+    infinite:true,
+    autoplay:true,
+    speed: 300,
+    slidesToShow:1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1 
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+  // slider
+  
+  $('.cus_rec3').slick({
+    dots: false,
+    arrows:true,
+    infinite:true,
+    autoplay:true,
+    speed: 300,
+    slidesToShow:3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1 
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  // slider 
+
+
+
+  // counter 
+
+  (function ($) {
+    $.fn.countTo = function (options) {
+      options = options || {};
+  
+      return $(this).each(function () {
+        // set options for current element
+        var settings = $.extend(
+          {},
+          $.fn.countTo.defaults,
+          {
+            from: $(this).data("from"),
+            to: $(this).data("to"),
+            speed: $(this).data("speed"),
+            refreshInterval: $(this).data("refresh-interval"),
+            decimals: $(this).data("decimals")
+          },
+          options
+        );
+  
+        // how many times to update the value, and how much to increment the value on each update
+        var loops = Math.ceil(settings.speed / settings.refreshInterval),
+          increment = (settings.to - settings.from) / loops;
+  
+        // references & variables that will change with each update
+        var self = this,
+          $self = $(this),
+          loopCount = 0,
+          value = settings.from,
+          data = $self.data("countTo") || {};
+  
+        $self.data("countTo", data);
+  
+        // if an existing interval can be found, clear it first
+        if (data.interval) {
+          clearInterval(data.interval);
+        }
+        data.interval = setInterval(updateTimer, settings.refreshInterval);
+  
+        // initialize the element with the starting value
+        render(value);
+  
+        function updateTimer() {
+          value += increment;
+          loopCount++;
+  
+          render(value);
+  
+          if (typeof settings.onUpdate == "function") {
+            settings.onUpdate.call(self, value);
+          }
+  
+          if (loopCount >= loops) {
+            // remove the interval
+            $self.removeData("countTo");
+            clearInterval(data.interval);
+            value = settings.to;
+  
+            if (typeof settings.onComplete == "function") {
+              settings.onComplete.call(self, value);
+            }
+          }
+        }
+  
+        function render(value) {
+          var formattedValue = settings.formatter.call(self, value, settings);
+          $self.html(formattedValue);
+        }
+      });
+    };
+  
+    $.fn.countTo.defaults = {
+      from: 0, // the number the element should start at
+      to: 0, // the number the element should end at
+      speed: 1000, // how long it should take to count between the target numbers
+      refreshInterval: 100, // how often the element should be updated
+      decimals: 0, // the number of decimal places to show
+      formatter: formatter, // handler for formatting the value before rendering
+      onUpdate: null, // callback method for every time the element is updated
+      onComplete: null // callback method for when the element finishes updating
+    };
+  
+    function formatter(value, settings) {
+      return value.toFixed(settings.decimals);
+    }
+  })(jQuery);
+  
+  jQuery(function ($) {
+    // custom formatting example
+    $(".count-number").data("countToOptions", {
+      formatter: function (value, options) {
+        return value
+          .toFixed(options.decimals)
+          .replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
+      }
+    });
+  
+    // start all the timers
+    $(".timer").each(count);
+  
+    function count(options) {
+      var $this = $(this);
+      options = $.extend({}, options || {}, $this.data("countToOptions") || {});
+      $this.countTo(options);
+    }
+  });
+  
